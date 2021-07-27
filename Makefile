a82724475cff810155fec74a74611ead9cdb72e7
user42 <bclerc@student.42.fr>
2021-07-27 20:07:37 +0200

    Makefile and clean code

---
+++ Makefile
@@ -0,0 +1,71 @@
+NAME = pushswap
+PROJECT_NAME = pushswap
+
+SOURCES_FOLDER = ./
+INCLUDES_FOLDER = includes/libft/
+
+OBJECTS_FOLDER = ./
+
+LIBS = libft
+
+SOURCES = \
+		./src/push_swap.c \
+		./src/clean.c \
+		./src/quicksort.c \
+		./src/small_sort.c \
+		./src/three_sort.c \
+		./src/utils.c \
+		./src/function/function_p.c \
+		./src/function/function_r.c \
+		./src/function/function_rr.c \
+		./src/function/function_s.c
+
+OBJECTS = $(SOURCES:.c=.o)
+
+FSANITIZE = -fsanitize=address
+FLAGS =
+CC = clang
+
+NO_COLOR =		\033[38;5;15m
+OK_COLOR =		\033[38;5;2m
+ERROR_COLOR =	\033[38;5;1m
+WARN_COLOR =	\033[38;5;3m
+SILENT_COLOR =	\033[38;5;245m
+INFO_COLOR =	\033[38;5;140m
+
+.PHONY: all re clean fclean libft force doclean
+
+all: $(NAME)
+
+$(LIBS):
+	@printf "$(NO_COLOR)All objects for $(INFO_COLOR)$(PROJECT_NAME) $(NO_COLOR)where successfully created.\n"
+	@printf "\n$(NO_COLOR)-------- $(INFO_COLOR)Libft $(NO_COLOR)--------\n"
+	@make -C includes/$(@)
+	@printf "$(NO_COLOR)-----------------------\n\n"
+
+$(NAME): $(OBJECTS) $(LIBS)
+	$(CC) $(CFLAGS) -o $(NAME) $(OBJECTS) -L$(INCLUDES_FOLDER) -lft
+	@printf "$(INFO_COLOR)$(NAME) $(NO_COLOR)successfully compiled. $(OK_COLOR)✓$(NO_COLOR)\n"
+
+force: $(OBJECTS)
+	@printf "$(NO_COLOR)All objects for $(INFO_COLOR)$(PROJECT_NAME) $(NO_COLOR)where successfully created.\n"
+	$(CC) $(CFLAGS) -o $(NAME) $(OBJECTS) -g
+	@printf "$(INFO_COLOR)$(NAME) $(NO_COLOR)successfully compiled. $(OK_COLOR)✓$(NO_COLOR)\n"
+
+%.o: %.c
+	@$(CC) $(FLAGS) -I$(INCLUDES_FOLDER) -c $< -o $@
+	@printf "$(NO_COLOR)Creating $(INFO_COLOR)%-30s $(OK_COLOR)✓$(NO_COLOR)\r" "$@"
+
+clean:
+	@make -C includes/$(LIBS) clean
+	@rm -f $(OBJECTS)
+	@printf "$(INFO_COLOR)$(PROJECT_NAME) $(NO_COLOR)Removed all objects$(NO_COLOR).\n"
+
+fclean: clean
+	@make -C includes/$(LIBS) fclean
+	@rm -f $(NAME)
+	@printf "$(INFO_COLOR)$(PROJECT_NAME) $(NO_COLOR)Removed $(INFO_COLOR)$(NAME)$(NO_COLOR).\n"
+
+doclean: all clean
+
+re: fclean all
--- src/push_swap.c
+++ src/push_swap.c
@@ -1,4 +1,4 @@
-	/* ************************************************************************** */
+/* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
 /*   push_swap.c                                        :+:      :+:    :+:   */
@@ -6,16 +6,16 @@
 /*   By: bclerc <bclerc@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/16 16:01:06 by bclerc            #+#    #+#             */
-/*   Updated: 2021/06/28 12:55:02 by bclerc           ###   ########.fr       */
+/*   Updated: 2021/07/27 19:43:21 by bclerc           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-# include "../includes/pushswap.h"
+#include "../includes/pushswap.h"
 
 // Y a pas de norme je sais
 
-t_stack	*lstnew(void const *value)
-{	
+t_stack *lstnew(void const *value)
+{
 	t_stack *list;
 	if (!(list = malloc(sizeof(t_list))))
 		return (NULL);
@@ -25,9 +25,9 @@ t_stack	*lstnew(void const *value)
 		list->next = NULL;
 		return (list);
 	}
-	if (!(list->value =(int)malloc(sizeof(int))))
+	if (!(list->value = (int)malloc(sizeof(int))))
 		return (NULL);
-	ft_memcpy((void*)list->value, value, 1);
+	ft_memcpy((void *)list->value, value, 1);
 	list->value = 0;
 	list->next = NULL;
 	return (list);
@@ -41,8 +41,9 @@ int add(int number, t_stack **stack)
 	new->value = number;
 	if (stack == NULL)
 	{
-		*stack = lstnew((void *)number);	
-		while(1);
+		*stack = lstnew((void *)number);
+		while (1)
+			;
 	}
 	if (stack != NULL && new != NULL)
 	{
@@ -67,39 +68,75 @@ int isNumber(char *number)
 	return (1);
 }
 
+int add_value_to_stack(t_stack *stack, t_stack *stack2)
+{
+	t_stack *tmp;
+
+	tmp = stack2;
+	while (tmp->next)
+		tmp = tmp->next;
+	tmp->next = stack;
+	return (1);
+}
+
+t_stack *createstack(int argc, char **argv)
+{
+	int i;
+	t_stack *list;
+	t_stack *tmp;
+
+	list = 0;
+	i = 1;
+	while (i < argc)
+	{
+
+		tmp = (t_stack *)malloc(sizeof(t_stack));
+		tmp->value = ft_atoi(argv[i]);
+		tmp->next = NULL;
+		if (!list)
+			list = tmp;
+		else
+		{
+			add_value_to_stack(tmp, list);
+		}
+		i++;
+	}
+
+	return (list);
+}
+
 int parse(char **argv, t_stack **stack, t_push *push)
 {
 	int i;
 	int x;
 
 	i = push->argc - 2;
-	while(i >= 0)
+	while (i >= 0)
 	{
-			if (isNumber(argv[i]))
-			{
-				add(ft_atoi(argv[i]), stack);
-				push->sizea++;
-				push->totalnumber++;
-			}
-			else
-			{
-				printf("%s is not a integer. Please use only integer", argv[i]);
-				return (0);
-			}
+		if (isNumber(argv[i]))
+		{
+			add(ft_atoi(argv[i]), stack);
+			push->sizea++;
+			push->totalnumber++;
+		}
+		else
+		{
+			printf("%s is not a integer. Please use only integer", argv[i]);
+			return (0);
+		}
 		i--;
 	}
 	return (1);
 }
 
-
 void readList(t_stack *stack)
 {
 	t_stack *tmp;
-	
+
 	if (stack == NULL)
 	{
 		printf("Empty stack\n");
-		return ;
+		return;
 	}
 	tmp = stack;
 	while (tmp->next)
@@ -108,33 +145,59 @@ void readList(t_stack *stack)
 		ft_putchar(' ');
 		tmp = tmp->next;
 	}
-		ft_putnbr(tmp->value);
-		ft_putchar(' ');
-		printf("\n");
+	ft_putnbr(tmp->value);
+	ft_putchar(' ');
+	printf("\n");
+}
+
+int patest(t_stack *stack, t_stack *stackb)
+{
+	t_stack *tmp;
+
+	if (!stack)
+		return 0;
+	tmp = stack;
+	stack = stack->next;
+	add(tmp->value, &stackb);
+	if (!stack)
+		stack = NULL;
+	return (1);
 }
 
 int main(int argc, char **argv)
 {
 	t_push push;
-	
+
 	push.argc = argc;
-	push.sizeb = 0;
-	push.sizea = 0;
 	push.totalnumber = 0;
 	if (argc < 2)
 	{
 		printf("Please insert a lot of numbers");
 		return (-1);
 	}
-	push.stacka = 0;
-	push.stackb = 0;
-	parse(argv + 1, &push.stacka, &push);
-	int b = 5;
-		readList(push.stacka);
-	if (argc < 5)
-	{
-		three_sort(&push);
-		readList(push.stacka);
-	}
+	push.stacka = createstack(argc, argv);
+	push.stackb = createstack(0, NULL);
+	readList(push.stacka);
+	readList(push.stackb);
+
+	//push.stackb = 0;
+	//parse(argv + 1, &push.stacka, &push);
+	//printf("Stack : a ");
+	//readList(push.stacka);
+	//printf("Stack : b ");
+	//readList(push.stackb);
+	//printf("\n ---- Après pointeur test ----\n");
+	//patest(&push.stacka, &push.stackb);
+	//printf("Stack : a ");
+	//readList(push.stacka);
+	//printf("Stack : b ");
+	//readList(push.stackb);
+
+	//int b = 5;
+	//if (argc < 5)
+	//{
+	//	three_sort(&push);
+	//	readList(push.stacka);
+	//}
 	return (1);
 }
